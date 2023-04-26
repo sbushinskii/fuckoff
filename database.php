a0f9f12da0dc55b49e175c211f3362917f988892
Sergey B <sbushinskii@gmail.com>
2023-04-27 00:03:24 +0300

    db

---
+++ database.php
@@ -0,0 +1,28 @@
+<?php
+
+class Database {
+    public $con;
+    public $error;
+    public function __construct()
+    {
+        $this->con = mysqli_connect("localhost", "u2036503_default", "yvgWrM6IT2ZVr66f", "u2036503_default");
+        if(!$this->con)
+        {
+            echo 'Database Connection Error ' . mysqli_connect_error($this->con);
+        }
+    }
+    public function insert($table_name, $data)
+    {
+        $string = "INSERT INTO ".$table_name." (";
+        $string .= implode(",", array_keys($data)) . ') VALUES (';
+        $string .= "'" . implode("','", array_values($data)) . "')";
+        if(mysqli_query($this->con, $string))
+        {
+            return true;
+        }
+        else
+        {
+            echo mysqli_error($this->con);
+        }
+    }
+}
