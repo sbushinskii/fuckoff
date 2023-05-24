f54b23b21feaf82b75af0a406a3e392ac751ffab
Sergey B <sbushinskii@gmail.com>
2023-05-24 22:52:46 +0300

    LOLАгсл

--- service/index.php
+++ service/index.php
@@ -14,7 +14,10 @@
                     <button class='btn btn-primary' onclick="document.location='/service/db-export.php'"  type='submit'>Экспорт БД</button>
                 </td>
                 <td>
-                    <button class='btn btn-primary' onclick="document.location='/service/scan.php?mode=light'" type='submit'>Обновить из облака</button>
+                    <button class='btn btn-primary' onclick="document.location='/service/scan.php?mode=light'" type='submit'>Обновить недавнее</button>
+                </td>
+                <td>
+                    <button class='btn btn-primary' onclick="document.location='/service/scan.php'" type='submit'>Обновить все</button>
                 </td>
                 <td>
                     <button class='btn btn-primary' onclick="document.location='/service/tags_counter.php'" type='submit'>Пересчет использования тегов</button>
